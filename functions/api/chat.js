135fdd3bbefe580d05ede5ba7271e3bc9d5ea2ce
Michaelair123 <michaelair123@gmail.com>
2026-09-19 17:33:36 -0700

    Fix lead token base64url encoding

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -50,7 +50,7 @@ function looksLikePreAiAbuse(text) {
 function base64UrlEncode(bytes) {
   let binary = "";
   for (const byte of bytes) binary += String.fromCharCode(byte);
-  return btoa(binary).replace(/\\+/g, "-").replace(/\\//g, "_").replace(/=+$/g, "");
+  return btoa(binary).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/g, "");
 }
 
 function base64UrlDecode(value) {
